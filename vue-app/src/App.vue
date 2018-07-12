52aeb7e9b915859704541e3fc78913cab2e593f1
Pier Bover <yo@pierbover.com>
2018-07-12 10:42:02 -0500

    First commit

---
+++ vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="App">
+		<h1>App</h1>
+		<test/>
+	</div>
+</template>
+
+<script>
+import Test from '../../shared-lib/Test.vue';
+
+export default {
+	components : {
+		'test': Test
+	}
+};
+</script>
---
+++ vue-app/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const ENTRY = './src/index.js';
+const OUTPUT = path.resolve(__dirname, '../dist/');
+
+module.exports = {
+	mode: process.env.NODE_ENV,
+	entry: ENTRY,
+	output: {
+		path: OUTPUT,
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.vue', '.json']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader',
+				include: [
+					path.resolve('../../shared-lib')
+				]
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin()
+	]
+};
